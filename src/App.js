4fd8710a29bfff2b947fbacfaecfaa9df48da3bc
vm5 <62893030+vm5@users.noreply.github.com>
2024-08-11 11:59:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import ConnectPESChatbot from './form';
-import Footer from './footer';
+
 function App() {
   return (
     <div className="App">
       <ConnectPESChatbot />
-      <Footer />
+
     </div>
   );
 }
